bfc84b5f9a688a41602ae83955d50c2baf3c6a3f
Laima <laiman@Laimas-MacBook-Pro.local>
2020-10-20 14:49:14 +0300

    initial commit

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Bootstrap project</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+
+  <nav class="navbar navbar-expand-lg fixed-top">
+      <div class="container">
+        
+          <a href="#" class="navbar-brand"><img src="./img/Logo.png" /></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" 
+          data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" 
+          aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+          </button>
+        
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Product</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Promo</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+          </ul>
+          <ul class="navbar-nav justify-content-end">
+            <li>
+              <a href="#"><i class="fas fa-search"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+  </nav>
---
+++ index.php
@@ -0,0 +1,256 @@
+<?php 
+  include_once 'header.php';
+?>
+    <!-- Section Hero -->
+    <div class="hero">
+      
+          <div class="jumbotron container">
+            <div class="row">
+              <div class="col-lg-4 col-md-6 col-sm-12">
+                <h1>DELICIOUS SUSHI</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                  lacinia odio vitae vestibulum vestibulum.
+                </p>
+                <button class="btn btn-success btn-lg">Order now</button>
+              </div>
+            </div>
+          </div>
+        
+    </div>
+
+    <!-- SECTION: BEST SELLER -->
+    <div class="bestseller">
+        <div class="container">
+            <div class="row bestseller-row1">
+              <div class="col-2"></div>
+              <div class="col-8 text-center">
+                <h2>BEST SELLER</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. </p>
+              </div>  
+              <div class="col-2"></div>  
+            </div>
+      
+
+          <div class="row bestseller-row2 text-center">
+            <div class="absolute-img1">
+              <img src="img/vector1.png" alt="">
+            </div>
+            
+              <div class="col-lg-4 sushi-plate ">
+                <img src="img/plate1.png" alt="">
+                <h4>Title Here</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+              <div class="col-lg-4 sushi-plate">
+                <img src="img/plate2.png" alt="">
+                <h4>Title Here</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+              <div class="col-lg-4 sushi-plate">
+                <img src="img/plate3.png" alt="">
+                <h4>Title Here</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+            
+            <div class="absolute-img2">
+              <img src="img/vector2.png" alt="">
+            </div>
+          </div>
+        </div> 
+    </div>
+   
+    <!-- Section Best vegan -->
+    <div class="bestvegan container"> 
+      <div class="row align-items-center justify-content-center">
+        <div class="col-lg-6 roundplate text-center">
+          <img src="img/round-img.png" alt="">
+        </div>
+        <div class="col-lg-6 vegan-text text-left">
+          <h2>BEST VEGAN</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. </p>
+          <button class="btn btn-success btn-lg">Order now</button>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- Section: Statistics -->
+    <div class="statistics">
+      <div class="container">
+        <div class="row statistics-row1">
+
+          <div  class="col-lg-3 col-sm-12 text-left shrimp-img"><img src="img/vector3.png" alt="shrimp-image"></div>
+
+          <div class="col-lg-6 col-sm-12 text-center">
+                <h2>STATISTICS</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.  </p>
+          </div>  
+
+          <div  class="col-lg-3 col-sm-12 text-right sushi-img"><img src="img/vector4.png" alt="sushi-image"></div>
+        </div>
+
+        <div class="row statistics-row2 text-center justify-content-center align-items-center">
+            <div class="col-lg-4 col-sm-12">
+              <div class="rounded-circle">
+                <div class="round-text"> 
+                  <h2>123</h2>
+                  <h3>STORE</h3>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-12">
+              <div class="rounded-circle">
+                <div class="round-text"> 
+                  <h2>100</h2>
+                  <h3>MENU</h3>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-12">
+              <div class=" rounded-circle">
+                <div class="round-text">
+                  <h2>200</h2>
+                  <h3>CHEF</h3>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="delicious-img3 row">
+          <img src="img/vector2.png" alt="">
+        </div>
+      </div>     
+    </div>
+
+
+    <!-- Section Delicious -->
+
+  
+    <div class="delicious container"> 
+      <div class="row absolute-imgs-row">
+        <div class="deliciuos-img1 col-lg-4"><img src="img/vector5.png" alt=""></div>
+        <div class="col-lg-4"></div>
+        <div class="deliciuos-img2"><img src="img/vector6.png" alt=""></div>
+      </div>
+      <div class="row align-items-center justify-content-center">
+        <div class="col-lg-6 delicious-text text-left">
+          <h2>DELICIOUS</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. </p>
+          <button class="btn btn-success btn-lg">Order now</button>
+        </div>
+        <div class="col-lg-6 roundplate text-center">
+          <img src="img/round-img2.png" alt="">
+        </div>
+      </div> 
+      <div class="row delicious-img4-row">
+        <div class="delicious-img4">
+          <img src="img/vector1.png" alt="">
+        </div>       
+      </div>
+    </div>  
+
+    <!-- Section About -->
+    <div class="about">
+      <div class="layer">
+        <div class="container text-center">
+          <div class="about-photo"><img src="img/photo.png" alt=""></div>
+          <div class="about-text"><p>If you are a culinary fan, if you like to spend time in your kitchen, you likely find yourself looking for reliable resources through which you can</p></div>
+          <div><h3>Phoebe Frazier</h3></div>
+        </div>  
+      </div>       
+    </div>
+
+    
+    <div class="subscribe">
+      
+        <div class="container text-center">
+          
+            <div class=""><h2>SUBSCRIBE</h2></div>
+            <div class="subscribe-text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. </p></div>
+          <form action="submit" class="subscribe-form">
+              <input type="text" placeholder="Email address" name="mail" required>             
+               <input type="submit" value="Subscribe" class="btn">   
+          </form>
+        </div>  
+        <div class="subscr-flex">
+          <div class="subscr-img1">
+            <img src="img/Vector2.png" alt="">
+          </div>
+          <div class="subscr-img2">
+            <img src="img/vector4.png" alt="">
+          </div>
+        </div>
+
+        <div class="container text-center subscr-img3">
+          <img src="img/vector3.png" alt="">
+        </div>                
+    </div>
+<!-- Section Contacts ----------- -->
+    <div class="contacts">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-right sushi-nigiri">
+            <img src="img/vector6.png" alt="sushi">
+          </div> 
+        </div>
+        <div class="row">
+            <div class="social col-lg-6 col-md-6">
+              <h4>Title Here</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at dignissim nunc, id maximus ex. Etiam nec dignissim elit, at dignissim enim. </p>
+              <div class="social-media">
+                <a href=""><img src="img/instagram.png" alt=""></a>
+                <a href=""><img src="img/facebook.png" alt=""></a>
+                <a href=""><img src="img/twitter.png" alt=""></a>
+                <a href=""><img src="img/whatsApp.png" alt=""></a>
+              </div>
+            </div>
+          
+          <div class="col-lg-2 col-md-2 history">
+              <h4>About</h4>
+              <ul>
+                <li><a href="#">History</a></li>
+                <li><a href="#">Our team</a></li>
+                <li><a href="#">Brand Guidelines</a></li>
+                <li><a href="#">Terms & Condition</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+          </div>
+          
+          
+            <div class="col-lg-2 col-md-2 services">
+            <h4>Services</h4>
+              <ul>
+                <li><a href="#">How to Order</a></li>
+                <li><a href="#">Our Product</a></li>
+                <li><a href="#">Order Status</a></li>
+                <li><a href="#">Promo</a></li>
+                <li><a href="#">Payment Method</a></li>
+              </ul>
+            </div>
+          
+        
+            <div class="col-lg-2 col-md-2 contact">
+              <h4>Other</h4>
+              <ul>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Privacy</a></li>
+              </ul>
+            </div>
+        
+        </div>
+
+
+
+        
+      </div>
+    </div>
+    
+
+
+
+
+<?php
+  include_once 'footer.php';
+?> 
